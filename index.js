43e3a03d4e968f3c9d98aed8caaa5a7e72396ae8
Alexis Barba <alexbarba.2278@gmail.com>
2019-06-29 11:30:16 -0600

    Hemos configurado el entorno de desarrollo, el puerto, añadido las dependencias graphql, express, el middleware para ambos, configurado los scripts del package.json, añadido los resolvers, el schema y probado algunos querys en la app.

---
+++ index.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const {buildSchema} = require("graphql");
+const express = require("express");
+const gqlMiddleware = require("express-graphql");
+const {readFileSync} = require("fs");
+const {join} = require("path");
+const resolvers = require("./lib/resolvers");
+const app = express();
+const port = process.env.port || 3000;
+
+//definiendo el esquema
+const schema = buildSchema(
+	readFileSync(join(__dirname, "lib", "schema.graphql"), "utf-8")
+);
+
+//Ejecutar el query hello
+// graphql(schema, "{ hello, saludo }", resolvers).then(data => {
+// 	console.log(data);
+// });
+
+app.use(
+	"/api",
+	gqlMiddleware({
+		schema: schema,
+		rootValue: resolvers,
+		graphiql: true
+	})
+);
+
+app.listen(port, () => {
+	console.log(`Server is listening at http://localhost:${port}/api`);
+});
