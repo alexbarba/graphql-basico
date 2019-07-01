81da52e423423651dae58bb2529a70a9eb74791c
Alexis Barba <alexbarba.2278@gmail.com>
2019-07-01 10:50:22 -0600

    Añadida la Query que devuelve un curso a través de un _id

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 "use strict";
 
-const {buildSchema} = require("graphql");
+const {makeExecutableSchema} = require("graphql-tools");
 const express = require("express");
 const gqlMiddleware = require("express-graphql");
 const {readFileSync} = require("fs");
 const {join} = require("path");
 const resolvers = require("./lib/resolvers");
+
 const app = express();
 const port = process.env.port || 3000;
 
-//definiendo el esquema
-const schema = buildSchema(
-	readFileSync(join(__dirname, "lib", "schema.graphql"), "utf-8")
+const typeDefs = readFileSync(
+	join(__dirname, "lib", "schema.graphql"),
+	"utf-8"
 );
+//definiendo el esquema
+const schema = makeExecutableSchema({typeDefs, resolvers});
 
 //Ejecutar el query hello
 // graphql(schema, "{ hello, saludo }", resolvers).then(data => {
